35c0ef4863c2d34cf23d23d94c0f086e6b980773
vagrant <vagrant@debian12.localdomain>
2024-10-11 18:10:17 +0000

    11th

--- main.c
+++ main.c
@@ -2,6 +2,10 @@
 
 int main(void)
 {
+<<<<<<< HEAD
    printf("printf - 9th split from main commit\n");
+=======
+   printf("printf - third commit\n");
+>>>>>>> a19f14b (third commit)
    return 0;
 }
